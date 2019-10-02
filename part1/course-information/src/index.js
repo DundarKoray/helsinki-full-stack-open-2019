4d6693130a6e4923f9275de3fdd266e7ee92e864
Koray Dundar <koraydundar_40@hotmail.com>
2019-10-02 17:01:24 +0300

    completed part 1.5

--- part1/course-information/src/index.js
+++ part1/course-information/src/index.js
@@ -1,12 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const Header = (props) => {
+    console.log(props)
+
+    return (
+        <header>
+            <h1>{props.course}</h1>
+        </header>
+    );
+};
+
+const Part = (props) => {
+    console.log(props)
+
+    return (
+        <p>{props.name} {props.exercises}</p>
+    )
+}
+
+
+const Content = (props) => {
+    console.log(props)
+    return (
+        <div>
+            {props.parts.map((item, index) => {
+                return <Part key={index} name={item.name} exercises={item.exercises} />
+            })}
+        </div>
+    );
+};
+
+
+const Total = (props) => {
+    console.log(props)
+
+    let number = props.parts.map(item => {
+        return item.exercises
+    })
+    console.log(number)
+
+    return (
+        <section>
+            <p>Number of exercises {number.reduce((acc, item) => {
+                return acc + item
+            })}</p>
+        </section>
+    );
+};
+
+
+
+
+const App = () => {
+    const course = {
+        name: "Half Stack application development",
+        parts: [
+
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
